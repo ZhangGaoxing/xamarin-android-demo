ec3e2da8bb58a3444b8de43f640cd9744d257651
ZhangGaoxing <993638176@qq.com>
2017-01-13 17:12:18 +0800

    HelloWorld
    An Introduction of Xamarin.Android

---
+++ HelloAndroid/HelloAndroid/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloAndroid
+{
+    [Activity(Label = "HelloAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 0;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // 加载布局
+            SetContentView (Resource.Layout.Main);
+            // 获取布局中的控件
+            Button say = FindViewById<Button>(Resource.Id.sayHello);
+            TextView show = FindViewById<TextView>(Resource.Id.showHello);
+            // 绑定 Click 事件
+            say.Click += (sender, e) =>
+            {
+                count++;
+                show.Text = "Hello, Android";
+                say.Text = $"You Clicked {count}";
+                // Toast 通知
+                Toast.MakeText(this, $"You Clicked {count}", ToastLength.Short).Show();
+            };
+        }
+    }
+}
+
